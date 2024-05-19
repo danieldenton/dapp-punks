96ae8f031a1482416b00783693e3ef9b46d7a11b
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-05-19 16:31:30 -0700

    initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,71 @@
-import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './Navigation';
-import Loading from './Loading';
+import Navigation from "./Navigation";
+import Loading from "./Loading";
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import NFT_ABI from "../abis/NFT.json";
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from "../config.json";
 
 function App() {
-  const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [provider, setProvider] = useState(null);
+  const [nft, setNft] = useState(null);
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [account, setAccount] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadBlockchainData = async () => {
     // Initiate provider
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const nft = new ethers.Contract(
+      config[31337].nft.address,
+      NFT_ABI,
+      provider
+    );
+    setNft(nft);
 
     // Fetch accounts
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
-
-    setIsLoading(false)
-  }
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     if (isLoading) {
-      loadBlockchainData()
+      loadBlockchainData();
     }
   }, [isLoading]);
 
-  return(
+  return (
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className="my-4 text-center">Dapp Punks</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
+          <p className="text-center">
+           
+          </p>
+          <p className="text-center">Edit App.js to add your code here.</p>
         </>
       )}
     </Container>
-  )
+  );
 }
 
 export default App;
